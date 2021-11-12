f1e7fc3918a63a95ba78d0b5d9b56508d9bae335
notN33T <52465709+notN33T@users.noreply.github.com>
2021-11-12 16:59:41 +0300

    Done header adminpage

--- client/src/pages/AdminComponents/HeaderAdmin/Header.js
+++ client/src/pages/AdminComponents/HeaderAdmin/Header.js
@@ -1,24 +1,17 @@
-import React from 'react'
-
+import React                from 'react'
 import './css/Header-a.css'
 
 
 export default function Header() {
-
-
-
     return(
         <header>
             <div className="admin-hdr">
-                
                 <div className="hd-prt-1">
-                    <h1>Animal Assistance</h1>
+                    <h1>Animal Assistance <span className="pg-inf">create post</span></h1>
                 </div>
-
                 <div className="hd-prt-2">
                     <button>Publish</button>
                 </div>
-
             </div>
         </header>
     )
